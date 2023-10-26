859cc1d6b9cdbb81da100fecb319562259d576a9
Danushka Nandasena <146468777+CodeWhi5per@users.noreply.github.com>
2023-10-26 05:51:48 +0530

    Delete src directory

--- src/main/java/com/IpayUsers/IpayUsers/Dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.IpayUsers.IpayUsers.Dao;
-
-import com.IpayUsers.IpayUsers.Domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User,Integer> {
-}
