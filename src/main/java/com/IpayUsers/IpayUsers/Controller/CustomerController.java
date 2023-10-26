4f3982085e1dce9a5ba8c687a2924da9fc17c899
CodeWhi5per <danushka.github@gmail.com>
2023-10-26 11:03:25 +0530

    1.1

--- src/main/java/com/IpayUsers/IpayUsers/Controller/CustomerController.java
+++ src/main/java/com/IpayUsers/IpayUsers/Controller/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/v1/customers")
+@RequestMapping(value = "api/v1/customer")
 @CrossOrigin
 public class CustomerController {
     @Autowired
